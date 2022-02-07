e3e36b3a16100a231a9c60e5b604884b72943b98
ABourdarie <alexandre.bourdarie@etu.unilim.fr>
2022-02-07 09:52:16 +0100

    ça ne marche absoluement pas mais ça compile

---
+++ ProjetPokeApi/Pages/ListPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Xamarin.Forms;
+using PokeApiNet;
+using System.Threading.Tasks;
+
+namespace ProjetPokeApi.Pages
+{
+
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+            PokemonsList.ToString();
+        }
+    }
+
+    public class PokemonsList
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+
+        
+    }
+
+    public class Pokemon
+    {
+        public static implicit operator Pokemon(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Title { get; set; }   
+           
+    }
+
+    public partial class PokemonsListViewModel
+    {
+
+    }
+
+    public class program
+    {
+        static HttpClient client = new HttpClient();
+
+        static async Task<Pokemon> GetPokemonAsync(string path)
+        {
+            Pokemon pokemon = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                pokemon = await response.Content.ReadAsStringAsync();
+            }
+            return pokemon;
+        }
+
+    }
+    
+}
---
+++ ProjetPokeApi/Pages/ShowPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjetPokeApi.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowPage : ContentPage
+    {
+        public ShowPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
