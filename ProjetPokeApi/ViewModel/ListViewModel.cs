ae1d2dd63eb7bd2f38532b26ff1b1919fb9dea40
ABourdarie <alexandre.bourdarie@etu.unilim.fr>
2022-02-16 15:46:39 +0100

    Commit du baseViewModel etc

---
+++ ProjetPokeApi/ViewModel/ListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PokeApiNet;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ProjetPokeApi.ViewModel
+{
+    internal class ListViewModel : BaseViewModel
+    {
+        public static ListViewModel _instance = new ListViewModel();
+        public static ListViewModel Instance => _instance;
+
+        public ObservableCollection<Pokemon> ListofPokemon
+        {
+            get => GetValue<ObservableCollection<Pokemon>>();
+            set
+            {
+                SetValue(value);
+            }
+        }
+    }
+}
